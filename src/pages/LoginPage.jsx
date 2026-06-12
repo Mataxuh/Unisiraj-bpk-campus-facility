2289041627537c0823faccd90930fa2fb402ee14
muhammad <Muhammadrabiumtz@gmail.com>
2026-06-12 16:20:59 +0800

    Fix: Remove BPK logo white background

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -102,7 +102,7 @@ const LoginPage = () => {
                 src="/bpk-logo.png"
                 alt="BPK"
                 className="h-16 sm:h-30 object-contain rounded-full bg-white p-1.5"
-                style={{ mixBlendMode: 'multiply' }}
+                style={{ mixBlendMode: 'screen' }}
               />
               <p className="text-xs font-bold" style={{ color: '#f0b145' }}>
                 BPK
